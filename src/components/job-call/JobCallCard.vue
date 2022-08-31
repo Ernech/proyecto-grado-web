b4d96b84356a4f43755697ab8100b1e24e105bef
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2022-08-31 01:01:46 -0400

    creating job call cards

---
+++ src/components/job-call/JobCallCard.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="card-container">
+        <h3 class="card-container__job-call-name">{{jobCallName}}</h3>
+        <span class="card-container__job-call-number">Convocatoria N°{{jobCallNumber}}</span>
+        <div class="card-container__opening-date">
+            <fa icon="fa-solid fa-calendar-days" class="opening-date__icon" />
+            <span class="opening-date__text">Fecha de apertura: {{openingDate}}</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        jobCallName:{type:String,required:true},
+        jobCallNumber:{type:String,required:true},
+        openingDate:{type:String,required:true}
+    }
+}
+</script>
+<style scoped>
+.card-container {
+    width: 65%;
+    height: 100px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.5);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 20px;
+    gap: 15px;
+}
+
+.card-container__job-call-name {
+
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    margin: 0px;
+}
+
+.card-container__job-call-number {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 13px;
+}
+
+.card-container__opening-date {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+}
+
+.opening-date__icon {
+    width: 25px;
+    height: 25px;
+}
+
+.opening-date__text {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+}
+.card-container:hover{
+    width: 66%;
+    height: 102px;
+    transition: 0.3s;
+}
+</style>
--- src/views/Recruiter/SavedJobCall.vue
+++ src/views/Recruiter/SavedJobCall.vue
@@ -1,13 +1,26 @@
 <template>
-    <div>
-        <h1>saved job calls</h1>
+    <div class="main">
+        <h3 class="title">Convocatorias guardadas</h3>
+        <JobCallCard jobCallName="ASISTENTE TÉCNICO DE MARKETING Y COMUNICACIÓN" jobCallNumber="06/2022" openingDate="24 de marzo de 2022"/>
+        <JobCallCard jobCallName="ASISTENTE DE LABORATORIO" jobCallNumber="07/2022" openingDate="24 de abril de 2022"/>
+        <JobCallCard jobCallName="AUXILIAR DE LABORATORIO" jobCallNumber="08/2022" openingDate="15 de mayo de 2022"/>
+        <JobCallCard jobCallName="SECRETARIO PARA BILIOTECA" jobCallNumber="10/2022" openingDate="24 de junio de 2022"/>
     </div>
 </template>
-<script>
-export default {
-    
-}
+<script setup>
+import JobCallCard from '../../components/job-call/JobCallCard.vue';
+
 </script>
 <style>
-    
+.main {
+    padding: 10px 50px;
+}
+
+.title {
+    font-family: 'Oswald', sans-serif;
+    font-style: normal;
+    font-size: 40px;
+    color: #064D90;
+    margin: 0px;
+}
 </style>
