8728d83b54d39a8025eee8e1ffcbf5ea28b44e64
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2022-11-08 00:48:54 -0400

    Creating cv accordions sections

---
+++ src/components/cv-accordion-sections/FamilyReferencesAccordion.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="accordion">
+        <div class="title" @click="changeAccordeonStatus">
+            <h3>Referencias laborales</h3>
+            <fa v-if="!isOpen" icon="fa-solid fa-chevron-down" class="arrow-icon"/>
+            <fa v-else icon="fa-solid fa-chevron-up" class="arrow-icon"/>
+        </div> 
+        <div class="content" v-if="isOpen">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, dicta, eius aliquid commodi sint similique ea ducimus facilis amet harum corrupti. Enim mollitia culpa dicta accusantium similique dolore quibusdam? Quod.</p>
+        </div>
+    </div>
+</template>
+<script setup>
+import {ref} from 'vue'
+const isOpen=ref(false)
+
+const changeAccordeonStatus=()=>{
+    if(isOpen.value){
+        isOpen.value=false
+        return
+    }
+    isOpen.value=true
+}
+</script>
+<style lang="scss" scoped>
+
+.accordion{
+    width: 100%;
+    
+}
+.title {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 20px;
+    gap: 650px;
+    border-top: rgb(166, 166, 166) solid 1px;
+    border-bottom: rgb(166, 166, 166) solid 1px;
+}
+
+.title h3 {
+    font-family: 'Inter';
+    font-size: 20px;
+    color: #000;
+}
+
+.content{
+    padding: 5px;
+    margin: 0;
+}
+.arrow-icon{
+    color: #000;
+    width: 40px;
+    height: 30px;
+}
+</style>
--- src/views/Recruiter/CVInfo.vue
+++ src/views/Recruiter/CVInfo.vue
@@ -2,22 +2,61 @@
     <div class="main">
         <div class="basic-info">
             <h3><b>Información del candidato</b></h3>
-          <h3><b>Nombre: </b>Ernesto Vilela Montero</h3>
+            <h3><b>Nombre: </b>Ernesto Vilela Montero</h3>
         </div>
         <div class="accordion-container">
-
+            <PersonalDataAccordion />
+            <AcademicTrainingAccordion />
+            <LanguagesAccordion />
+            <CurrentProfessionalAccordion />
+            <JobExperienceAccordion />
+            <TeachingExperienceAccordion />
+            <CoursesAndSeminarsAccordion />
+            <PublicationsAccordion />
+            <ConsultingsAndResearchsAccordion/>
+            <AwardsAccordion/>
+            <AffiliationsAccordion/>
+            <JobReferencesAccordion/>
+            <FamilyReferencesAccordion/>
         </div>
     </div>
 </template>
 <script>
+import PersonalDataAccordion from '../../components/cv-accordion-sections/PersonalDataAccordion.vue';
+import AcademicTrainingAccordion from '../../components/cv-accordion-sections/AcademicTrainingAccordion.vue';
+import LanguagesAccordion from '../../components/cv-accordion-sections/LanguagesAccordion.vue'
+import CurrentProfessionalAccordion from '../../components/cv-accordion-sections/CurrentProfessionalAccordion.vue'
+import JobExperienceAccordion from '../../components/cv-accordion-sections/JobExperienceAccordion.vue'
+import TeachingExperienceAccordion from '../../components/cv-accordion-sections/TeachingExperienceAccordion.vue'
+import CoursesAndSeminarsAccordion from '../../components/cv-accordion-sections/CoursesAndSeminarsAccordion.vue'
+import PublicationsAccordion from '../../components/cv-accordion-sections/PublicationsAccordion.vue'
+import ConsultingsAndResearchsAccordion from '../../components/cv-accordion-sections/ConsultingsAndResearchsAccordion.vue'
+import AwardsAccordion from '../../components/cv-accordion-sections/AwardsAccordion.vue'
+import AffiliationsAccordion from '../../components/cv-accordion-sections/AffiliationsAccordion.vue'
+import JobReferencesAccordion from '../../components/cv-accordion-sections/JobReferencesAccordion.vue'
+import FamilyReferencesAccordion from '../../components/cv-accordion-sections/FamilyReferencesAccordion.vue'
 export default {
-
+    components: {
+        PersonalDataAccordion,
+        AcademicTrainingAccordion,
+        LanguagesAccordion,
+        CurrentProfessionalAccordion,
+        JobExperienceAccordion,
+        TeachingExperienceAccordion,
+        CoursesAndSeminarsAccordion,
+        PublicationsAccordion,
+        ConsultingsAndResearchsAccordion,
+        AwardsAccordion,
+        AffiliationsAccordion,
+        JobReferencesAccordion
+    }
 }
 </script>
 <style scoped lang="scss">
 .main {
     padding: 10px 50px;
 }
+
 .basic-info {
     display: flex;
     flex-direction: column;
@@ -27,6 +66,7 @@ export default {
     gap: 10px;
 
 }
+
 h3 {
     font-family: 'Inter';
     font-style: normal;
@@ -34,7 +74,8 @@ h3 {
     font-size: 20px;
     margin: 0px;
 }
-.accordion-container{
+
+.accordion-container {
     width: 100%;
 }
 </style>
