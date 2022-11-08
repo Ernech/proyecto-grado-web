a1dab41ce50b9d996cd92868d2052e54ad0471d6
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2022-11-08 01:17:01 -0400

    Adding scss file

--- src/components/cv-accordion-sections/PublicationsAccordion.vue
+++ src/components/cv-accordion-sections/PublicationsAccordion.vue
@@ -23,35 +23,5 @@ const changeAccordeonStatus=()=>{
 }
 </script>
 <style lang="scss" scoped>
-
-.accordion{
-    width: 100%;
-    
-}
-.title {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    padding: 10px 20px;
-    gap: 650px;
-    border-top: rgb(166, 166, 166) solid 1px;
-    border-bottom: rgb(166, 166, 166) solid 1px;
-}
-
-.title h3 {
-    font-family: 'Inter';
-    font-size: 20px;
-    color: #000;
-}
-
-.content{
-    padding: 5px;
-    margin: 0;
-}
-.arrow-icon{
-    color: #000;
-    width: 40px;
-    height: 30px;
-}
+@import '../../styles/accordion.scss';
 </style>
